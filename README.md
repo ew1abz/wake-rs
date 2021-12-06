8dcbcd682a1623d4680ff7df766fd3439b71bd07
Vladimir K <ew1abz@gmail.com>
2021-12-06 01:53:27 -0800

    v0.2.2-rc1

--- README.md
+++ README.md
@@ -1,3 +1,70 @@
-# wake
+# wakers
 
-Wake protocol library written in Rust. Protocol description and tools: <http://www.leoniv.diod.club/articles/wake/wake.html>
+`wakers` is a library written in Rust for encoding/decoding Wake protocol packets.
+
+`Wake` is a serial communication protocol highly optimized for **microcontrollers**. It based on SLIP protocol (<https://datatracker.ietf.org/doc/html/rfc1055>).
+
+![debug_print](images/debug_print.png)
+
+## Main features
+
+- unique start symbol
+- 7-bit addressing (optional)
+- CRC (8 or 16 bits)
+- low overhead
+
+The protocol doesn't support:
+
+- ~~error correction~~
+- ~~compression~~
+
+Frame structure:
+
+![Frame structure](images/wake.png)
+
+## Integrations
+
+There are many architecture-specific implementations:
+
+|
+- MCS-51
+- AVR
+- STM32
+- x86
+|
+
+in many languages:
+
+- C
+- C++
+- C#
+- Python
+- Rust
+
+## Examples
+
+1. Demo - basic usage
+2. Serial - how to use with serial port
+3. Relay shield - real device communication
+
+## Build
+
+### Library
+
+```bash
+cargo build --release
+```
+
+### Examples
+
+```bash
+cargo build --examples
+```
+
+## Resources
+
+Protocol description, libraries, and tools: <http://www.leoniv.diod.club/articles/wake/wake.html>
+
+## License
+
+Code released under the MIT License.
---
+++ examples/2-serial.rs
@@ -0,0 +1,67 @@
+//! This example shows how to use wake_rs library along with serial port.
+//! 1. Connect RX and TX pins.
+//! 2. Run this example `cargo run --example 2-serial`
+
+extern crate serialport;
+extern crate wakers;
+
+use std::io::Write;
+use std::thread;
+use std::time::Duration;
+use wakers::{Decode, Encode};
+
+fn print_packet(header: &str, v: Option<&Vec<u8>>) {
+    print!("\n{}:\t", header);
+    match v {
+        Some(data) => {
+            for x in data {
+                print!("{:02X} ", x);
+            }
+        }
+        None => print!("[]"),
+    }
+}
+
+fn main() {
+    let cmd_version = wakers::Packet {
+        address: None,
+        command: 0x01,
+        data: None,
+    }
+    .encode();
+
+    let cmd_start = wakers::Packet {
+        address: None,
+        command: 0x02,
+        data: Some(vec![10, 10]),
+    }
+    .encode();
+
+    let cmd_stop = wakers::Packet {
+        address: None,
+        command: 0x02,
+        data: Some(vec![0, 0]),
+    }
+    .encode();
+    let mut commands = [cmd_version, cmd_start, cmd_stop];
+
+    let mut port = serialport::new("COM4", 9600)
+        .timeout(Duration::from_millis(100))
+        .open()
+        .expect("Failed to open port");
+
+    let mut rx: Vec<u8> = vec![0; 64];
+    let mut state: usize = 0;
+    loop {
+        port.write(commands[state].as_mut_slice())
+            .expect("failed to write message");
+        let n = port.read(rx.as_mut_slice()).unwrap();
+        print_packet("RAW RX", Some(&rx[..n].to_vec()));
+        let d = &rx[..n].to_vec().decode().unwrap();
+        print!("\nDecoded CMD {}", d.command);
+        print_packet("Decoded data", d.data.as_ref());
+        state = if state >= 2 { 0 } else { state + 1 };
+        print!("\n------------");
+        thread::sleep(Duration::from_millis(5000));
+    }
+}
